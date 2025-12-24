805bdd9c8df68911804cdf37bb35a72cfd4aa37e
Dejan Zivkovic <dejan.zivkovic90@gmail.com>
2025-12-24 16:14:04 +0100

    add nginx setup (not completed)

--- local-dev-setup.sh
+++
@@ -1,8 +0,0 @@
-# /bin/sh
-
-helmfile destroy
-kubectl delete pvc --all
-helmfile apply
-kubectl port-forward svc/microservices-items 3001:3000 &
-kubectl port-forward svc/postgres-release-postgresql 5433:5432 &
-
